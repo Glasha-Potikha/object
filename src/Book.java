287d39cf8e632d0b5c1f6d0273d5519ae5f5d152
Glashaty <firatiha7404@gmail.com>
2024-11-25 16:34:50 +0300

    HW - object methods

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     final private String title;
     final private Author name;
@@ -27,5 +29,23 @@ public class Book {
         } else {
             this.publishYear = publishYear;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " «" + title + "»";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@ public class Main {
     public static void main(String[] args) {
         Author joanKR = new Author("Джоан", "Роулинг");
         Book harry3 = new Book("Гарри Поттер и узник Азкабана", joanKR, 1999);
+        Book HP3 = new Book("Гарри Поттер и узник Азкабана", joanKR, 2010);
 
         Author arthurCD = new Author("Артур", "Конан Дойл");
         Book sherlock1 = new Book("Этюд в багровых тонах", arthurCD, 1887);
 
-        System.out.println(joanKR.getName() + " " + joanKR.getSurname() + " написала произведение «" + harry3.getTitle() + "»");
+        System.out.println(arthurCD.getName());
+        System.out.println(harry3.getName() + " " + joanKR.getSurname() + " написала произведение «" + harry3.getTitle() + "»");
         System.out.println("Первое издание книги «" + sherlock1.getTitle() + "» вышло в " + sherlock1.getPublishYear() + " году");
 
+
         sherlock1.setPublishYear(2000);
-        System.out.println("У нас есть издание " + sherlock1.getPublishYear() + " года «" + sherlock1.getTitle() + "» за авторством " + sherlock1.getName().getFullName());
+        System.out.println(sherlock1.getPublishYear());
+
+        System.out.println(joanKR);
+        System.out.println(sherlock1);
+        System.out.println(sherlock1.equals(harry3));
+        System.out.println(harry3.equals(HP3));
     }
 }
